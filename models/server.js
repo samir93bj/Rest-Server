17e35f1fbba345a57addc2b045779a48d6909814
samir93bj <samir93bj@gmail.com>
2022-02-21 00:52:09 -0300

    changes in the variable env

--- models/server.js
+++ models/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 const { dbConnection } = require('../database/config');
-
+ 
 class Server {
 
     constructor(){
