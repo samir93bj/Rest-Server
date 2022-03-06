22bfe632de0eded06b1f911a886e85303aa44008
samir93bj <samir93bj@gmail.com>
2022-03-06 19:05:20 -0300

    bug fix prod

--- helpers/google-verify.js
+++ helpers/google-verify.js
@@ -4,6 +4,7 @@ const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 
 const googleVerify = async(idToken = '') => {
 
+  console.log(idToken);
   const ticket = await client.verifyIdToken({
       idToken,
       audience: process.env.GOOGLE_CLIENT_ID,
